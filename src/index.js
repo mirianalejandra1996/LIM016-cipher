b382ed36e083990ae04a859c1e1cd49a96b47817
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-10-17 10:59:55 -0500

    una sola ventana y todo casi funcional

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ enterDecodedInput.addEventListener('input', ()=>{cipher.codificar(enterDecodedIn
 const changeWindowsToDecoded = document.getElementById('change-windows-to-decoded');
 changeWindowsToDecoded.addEventListener('click', functionalitiesEncodedToDecoded.changeToDecoded);
 
-const cleanInputs = document.getElementById('clean-decoded-input');
-cleanInputs.addEventListener('click', functionalitiesEncodedToDecoded.limpiarVentanaDecodificada);
+const cleanDecodedInput = document.getElementById('clean-decoded-input');
+cleanDecodedInput.addEventListener('click', functionalitiesEncodedToDecoded.limpiarVentanaDecodificada);
 
 decodedOffsetInput.addEventListener('input', () => {functionalitiesEncodedToDecoded.encodedOffsetChanges(enterDecodedInput,decodedOffsetInput)});
 
@@ -21,11 +21,15 @@ decodedOffsetInput.addEventListener('input', () => {functionalitiesEncodedToDeco
 
 // Ventana encoded-to-decoded
 
+// let enterEncodedInput, encodedOffsetInput, cleanEncodedInput;
 
-let enterEncodedInput = document.getElementById('ingresar-texto-codificado');
-const encodedOffsetInput = document.getElementById('decoded-offset');
 
-enterEncodedInput.addEventListener('input', ()=>{cipher.decodificar(enterEncodedInput,encodedOffsetInput)});
+
+
+// let enterEncodedInput = document.getElementById('ingresar-texto-codificado');
+// const encodedOffsetInput = document.getElementById('decoded-offset');
+
+// enterEncodedInput.addEventListener('input', ()=>{cipher.decodificar(enterEncodedInput,encodedOffsetInput)});
 
 const changeWindowsToEncoded = document.getElementById('change-windows-to-encoded');
 changeWindowsToEncoded.addEventListener('click', functionalitiesDecodedToEncoded.changeToEncoded);
