35a0a7c6ce29153be1ec1e1087a87a929b5ba788
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-10-19 14:17:46 -0500

    funcionalidades hechas, solo falta offset negativo y efectos

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,3 @@
-// Codigo para decodificar
-// let decodedFormula = (letterDecodedPosition - 65 - chosenOffset ) % 26 + 65;
-
 function codificar (text, offset) {
   
   console.log('tecleando en codificar');
@@ -66,36 +63,43 @@ function decodificar (text, offset) {
   
   console.log('si tiene elementos este array');
 
-    let encodedTextResult = '';
-    // ! NO TOCAR
-    for (let letter of encodedArray){
+  let encodedTextResult = '';
+  // ! NO TOCAR
+  for (let letter of encodedArray){
 
-        let letterDecodedPosition = letter.charCodeAt(0);
+      let letterDecodedPosition = letter.charCodeAt(0);
 
-        // Están consideradas solamente las letras que conforman el abecedario en mayúscula y en minúscula.
+      console.log(`la letra ${letter} está en la posición ${letterDecodedPosition}`);
 
-        if (letterDecodedPosition >= 65 && letterDecodedPosition <= 90){
-          
-          // si la letra es mayúscula
-          let decodedFormula = (letterDecodedPosition - 65 - chosenOffset ) % 26 + 65;
-          let upperCaseLetter = String.fromCharCode(decodedFormula);
-          encodedTextResult+=upperCaseLetter;
+      // Están consideradas solamente las letras que conforman el abecedario en mayúscula y en minúscula.
 
-        } else if (letterDecodedPosition >= 97 && letterDecodedPosition <= 122){
+      if (letterDecodedPosition >= 65 && letterDecodedPosition <= 90){
+        
+        // si la letra es mayúscula
 
-          // si la letra es minúscula
-          let decodedFormula = (letterDecodedPosition - 97 + chosenOffset ) % 26 + 97; //CODIFICAR
-          
-          let encodedLetter = String.fromCharCode(decodedFormula);
-          encodedTextResult+=encodedLetter;
+        let decodedFormula = (letterDecodedPosition - 65 + (26 - chosenOffset )) % 26 + 65;
+        let upperCaseLetter = String.fromCharCode(decodedFormula);
 
-        } else{
-          encodedTextResult+=letter;
-        }
+        console.log(`La letra ${upperCaseLetter} está en la posición ${upperCaseLetter.charCodeAt(0)}`);
+        encodedTextResult+=upperCaseLetter;
+        
 
+      } else if (letterDecodedPosition >= 97 && letterDecodedPosition <= 122){
+
+        // si la letra es minúscula
+        let decodedFormula = (letterDecodedPosition - 97 + (26 - chosenOffset ) ) % 26 + 97; //CODIFICAR
+        
+        let encodedLetter = String.fromCharCode(decodedFormula);
+        encodedTextResult+=encodedLetter;
+
+      } else{
+        encodedTextResult+=letter;
       }
-      
-    return encodedTextResult;
+
+      // console.log(`la letra ${letter} está en la posición ${letterDecodedPosition} y se traduce a ${encodedLetter} en la posición ${encodedLetter.charCodeAt(0)}`);
+  
+  }
+  return encodedTextResult;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import cipher from './cipher.js';
-import { functionalitiesEncodedToDecoded, functionalitiesDecodedToEncoded } from './btnsFunctionality.js';
-
 
 // Ventana decoded-to-encoded
-let isEncode = true//puede tomar valores: 'codificar'o 'descodificar'
+
+let isEncode = true //puede tomar valores: 'codificar'o 'descodificar'
 let message = document.getElementById('texto');
-const offset = document.getElementById('encoded-offset');
+const offset = document.getElementById('offset');
 
 const codeOrEncode = () => {
-    let decodedTex = isEncode ? cipher.codificar(message, offset) : cipher.decodificar(message, offset)
+
+    let decodedTex = isEncode ? cipher.codificar(message, offset) : cipher.decodificar(message, offset);
     
     let resultado = document.getElementById('resultado');
     resultado.value = decodedTex;
@@ -22,62 +22,39 @@ offset.addEventListener('input', codeOrEncode);
 const toggleBtn = document.getElementById('change-windows');
 
 toggleBtn.addEventListener('click', () => {
-    isEncode = !isEncode
-    console.log('isEncode: ', isEncode);
-    codeOrEncode()
+    isEncode = !isEncode;
+    // console.log('isEncode: ', isEncode);
+    
+    let upperBtn = document.getElementById('upper-btn');
+    let lowerBtn = document.getElementById('lower-btn');
+
+    let lowerTab = document.getElementById('lower-tab');
+    let upperTab = document.getElementById('upper-tab');
+
+    message.value = '';
+    offset.value = 0;
+
+    if (isEncode){
+        upperBtn.className = 'icon-a-letter-icon';
+        lowerBtn.className = 'icon-chinese-letter-icon';
+        upperTab.innerHTML = 'Decodificado';
+        lowerTab.innerHTML = 'Codificado';
+    } else {
+        upperBtn.className = 'icon-chinese-letter-icon';
+        lowerBtn.className = 'icon-a-letter-icon';
+        upperTab.innerHTML = 'Codificado';
+        lowerTab.innerHTML = 'Decodificado';
+    }
+
+    codeOrEncode();
 
 });
 
-const cleanInput = document.getElementById('clean-decoded-input');
+const cleanInput = document.getElementById('clean');
+
 cleanInput.addEventListener('click', () => {
     document.getElementById('texto').value = '';
     document.getElementById('resultado').value = '';
 
 
 });
-
-
-// ----------------------------------
-
-
-// Ventana encoded-to-decoded
-
-// let enterEncodedInput = document.getElementById('ingresar-texto-codificado');
-// const encodedOffsetInput = document.getElementById('decoded-offset');
-
-
-// enterEncodedInput.addEventListener('input', ()=>{
-
-//     let encodedText = cipher.decodificar(enterEncodedInput,encodedOffsetInput);
-
-//     let resultadoDecodificado=document.getElementById('resultado-decodificado');
-//     resultadoDecodificado.value = encodedText;
-
-// });
-
-// const changeWindowsToEncoded = document.getElementById('change-windows-to-encoded');
-// changeWindowsToEncoded.addEventListener('click', functionalitiesDecodedToEncoded.changeToEncoded);
-
-// const cleanEncodedInput = document.getElementById('clean-encoded-input');
-// cleanEncodedInput.addEventListener('click', functionalitiesDecodedToEncoded.limpiarVentanaCodificada);
-
-// encodedOffsetInput.addEventListener('input', () => {
-
-//     // console.log('inpuuuut');
-
-//     // let decodedText = cipher.decodificar(enterEncodedInput,encodedOffsetInput);
-
-//     // let resultadoCodificado = document.getElementById('resultado-codificado');
-//     // resultadoCodificado.value = decodedText;
-// });
-
-
-
-
-
-
-// function probandoDecodificar (text,offset){
-//     cipher.decodificar(text,offset);
-// }
-
-// probandoDecodificar('HI JK','7');
